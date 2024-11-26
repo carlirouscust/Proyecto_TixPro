c217bd095004cc35406e1fd7b6cd665dccc3af1c
Alma Ozoria <heisylior@hotmail.com>
2024-11-26 17:08:14 -0400

    Identando ticket

--- Proyecto_TixPro/Models/Ticket.cs
+++ Proyecto_TixPro/Models/Ticket.cs
@@ -4,11 +4,11 @@ namespace Proyecto_TixPro.Models;
 
 public class Ticket
 {
-         [Key]
-        public int TicketId { get; set; } 
-        public int EventoId { get; set; } 
-        public double Precio { get; set; } 
-        public DateTime FechaCompra { get; set; } 
-        public int UsuarioId { get; set; } 
-    
+    [Key]
+    public int TicketId { get; set; } 
+    public int EventoId { get; set; } 
+    public double Precio { get; set; } 
+    public DateTime FechaCompra { get; set; } 
+    public int UsuarioId { get; set; } 
+
 }
