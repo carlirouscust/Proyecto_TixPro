4d17766e6dedefc983199fd7a09f10200e718006
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-26 16:16:00 -0400

    probando live share

--- Proyecto_TixPro/Services/ClientesService.cs
+++ Proyecto_TixPro/Services/ClientesService.cs
@@ -1,6 +1,6 @@
-﻿namespace Proyecto_TixPro.Services;
+﻿using Microsoft.EntityFrameworkCore;
+namespace Proyecto_TixPro.Services;
 
 public class ClientesService
 {
-    public int ID { get; set; }
 }
