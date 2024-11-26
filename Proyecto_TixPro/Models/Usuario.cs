4a6660e5f56061af39108ab01108bc199699ddb2
Alma Ozoria <heisylior@hotmail.com>
2024-11-26 16:50:32 -0400

    Cambiandole el nombre de Clientes a Usuario

---
+++ Proyecto_TixPro/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_TixPro.Models;
+
+
+public class Usuario
+{
+    [Key]
+    public int UsuarioId { get; set; }
+    public string? Nombre { get; set; }
+    public string? Email { get; set; }
+    public string? Contraseña { get; set; }
+}
