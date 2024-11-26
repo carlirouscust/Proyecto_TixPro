299073dd8aea8e94b94550f8e9b67938b5d66d01
Alma Ozoria <heisylior@hotmail.com>
2024-11-26 17:29:29 -0400

    Creando el modelo de Evento

---
+++ Proyecto_TixPro/Models/Evento.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_TixPro.Models;
+
+public class Evento
+{
+    [Key]
+    public int eventoId { get; set; }
+    public string? nombre { get; set; }
+    public DateTime fecha { get; set; }
+    public string? lugar { get; set; }
+    public decimal precio { get; set; }
+}
