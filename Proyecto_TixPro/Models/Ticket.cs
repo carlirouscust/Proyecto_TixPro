c05cefd093bd553d7c502f65a6068f98a2044c70
Alma Ozoria <heisylior@hotmail.com>
2024-11-26 17:49:00 -0400

    Agregando Forenkey

--- Proyecto_TixPro/Models/Ticket.cs
+++ Proyecto_TixPro/Models/Ticket.cs
@@ -1,14 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Proyecto_TixPro.Models;
 
 public class Ticket
 {
-        [Key]
-        public int ticketId { get; set; } 
-        public int eventoId { get; set; } 
-        public double precio { get; set; } 
-        public DateTime fechaCompra { get; set; } 
-        public int usuarioId { get; set; } 
+    [Key]
+    public int ticketId { get; set; }
+
+    public Evento? evento { get; set; }
+    [ForeignKey("evento")]
+    public int eventoId { get; set; } 
+    public double precio { get; set; } 
+    public DateTime fechaCompra { get; set; } 
+
+    public Usuario? usuario { get; set; }
+    [ForeignKey("usuario")]
+    public int usuarioId { get; set; } 
     
 }
