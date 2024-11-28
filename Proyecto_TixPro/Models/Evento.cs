90d1a1467b1f8321b6b62d6b819f6231a4b9a5ec
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-27 20:19:12 -0400

    se modifico el modelo evento

--- Proyecto_TixPro/Models/Evento.cs
+++ Proyecto_TixPro/Models/Evento.cs
@@ -7,18 +7,18 @@ public class Evento
     [Key]
     public int eventoId { get; set; }
 
-    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    //[Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
     public string? nombre { get; set; }
 
-    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
-    public DateTime fecha { get; set; }
+    //[Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    public DateTime fecha { get; set; } = DateTime.Now;
 
-    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+   // [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
     public string? lugar { get; set; }
 
-    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    //[Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
     public decimal? precio { get; set; }
 
-    [Required(ErrorMessage = "Este campo debe ser obligatorio")]
+    //[Required(ErrorMessage = "Este campo debe ser obligatorio")]
     public string? imagen { get; set; }
 }
