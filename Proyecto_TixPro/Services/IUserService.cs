c00618529b1403388988cdc2b5f1dae538928589
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-28 19:44:34 -0400

    para mostrar la ui

---
+++ Proyecto_TixPro/Services/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace Proyecto_TixPro.Services;
+
+public interface IUserService
+{
+    ClaimsPrincipal? GetCurrentUser();
+}
+public class UserService : IUserService
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public UserService(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+
+    public ClaimsPrincipal? GetCurrentUser()
+    {
+        return _contextAccessor.HttpContext?.User;
+    }
+}
