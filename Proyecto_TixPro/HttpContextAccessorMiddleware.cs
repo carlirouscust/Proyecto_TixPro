7d39f57b5f0fb0fc4ed9f1888e36499ca70b6e6d
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-28 19:44:49 -0400

    clase http context

---
+++ Proyecto_TixPro/HttpContextAccessorMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace Proyecto_TixPro;
+
+public class HttpContextAccessorMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public HttpContextAccessorMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, IHttpContextAccessor httpContextAccessor)
+    {
+        // Asigna el HttpContext actual al IHttpContextAccessor
+        httpContextAccessor.HttpContext = context;
+
+        // Llama al siguiente middleware en la tubería
+        await _next(context);
+    }
+}
