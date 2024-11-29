bc3203d02ed62ccd71cfdcf7dec22e1cd874f3a7
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-11-29 01:37:46 -0400

    se creo el modelo cobros

---
+++ Proyecto_TixPro/Models/Cobros.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto_TixPro.Models;
+
+public class Cobros
+{
+    [Key]
+    public int cobrosId { get; set; }
+
+    public decimal totalPagado { get; set; }
+
+    public Usuario? usuario { get; set; }
+    [ForeignKey("usuario")]
+    public int usuarioId { get; set; }
+
+    public Evento? evento { get; set; }
+    [ForeignKey("evento")]
+    public int eventoId { get; set; }
+}
