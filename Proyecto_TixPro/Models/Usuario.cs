9bd0741cefbfa5c282946c2243a2453b41c4a90c
Alma Ozoria <heisylior@hotmail.com>
2024-11-30 03:09:31 -0400

    poniendo alarma

--- Proyecto_TixPro/Models/Usuario.cs
+++ Proyecto_TixPro/Models/Usuario.cs
@@ -15,8 +15,10 @@ public class Usuario
     [Required(ErrorMessage = "El Whatsapp es obligatorio")]
     public string? whatsapp { get; set; }
 
-     public string? asunto { get; set; }
+    [Required(ErrorMessage = "El Asunto es obligatorio")]
+    public string? asunto { get; set; }
 
+    [Required(ErrorMessage = "El Comentario es obligatorio")]
     public string? comentario { get; set; }
     public  ICollection<Ticket> Ticket { get; set; } = new List<Ticket>();
 }
