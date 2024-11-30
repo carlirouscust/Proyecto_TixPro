9afaead2764e6863f163bb9a8dddbf5f82ea6f6d
Alma Ozoria <heisylior@hotmail.com>
2024-11-29 21:29:21 -0400

    Creando modelo tarjeta

---
+++ Proyecto_TixPro/Models/Tarjeta.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace Proyecto_TixPro.Models;
+
+public class Tarjeta
+{
+    [Key]
+    public int tarjetaId { get; set; }
+    public string? nombreTitular { get; set; }
+    public string? numeroTarjeta { get; set; }
+    public string fechaExpiracion { get; set; }
+    public string? codigoSeguridad { get; set; }
+}
