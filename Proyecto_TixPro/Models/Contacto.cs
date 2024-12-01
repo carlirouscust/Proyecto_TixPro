64004c634d93ed43dc21f7890f2e869dcefbb5d3
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-12-01 03:40:04 -0400

    se creo el modelo contacto

---
+++ Proyecto_TixPro/Models/Contacto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_TixPro.Models;
+
+public class Contacto
+{
+    [Key]
+    public int contactoId { get; set; }
+
+    [Required(ErrorMessage = "El Asunto es obligatorio")]
+    public string? asunto { get; set; }
+
+    [Required(ErrorMessage = "El Comentario es obligatorio")]
+    public string? comentario { get; set; }
+
+    [Required(ErrorMessage = "El Comentario es obligatorio")]
+    public string? nombre { get; set; }
+
+    [Required(ErrorMessage = "El Comentario es obligatorio")]
+    public string? whatsapp {  get; set; }
+}
