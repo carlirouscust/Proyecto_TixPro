65b9a1af8cd00c6d80cfcdc8a4ca292acdcdf849
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-12-01 03:40:21 -0400

    se puso usuario en modelo tarjeta para que tenga la relacion de uno a uno

--- Proyecto_TixPro/Models/Tarjeta.cs
+++ Proyecto_TixPro/Models/Tarjeta.cs
@@ -7,8 +7,19 @@ public class Tarjeta
 {
     [Key]
     public int tarjetaId { get; set; }
+
+    [Required(ErrorMessage = "El Nombre del titular es Ogligatorio")]
     public string? nombreTitular { get; set; }
+
+    [Required(ErrorMessage = "El Numero de tarjeta es Obligatorio")]
     public string? numeroTarjeta { get; set; }
-    public string fechaExpiracion { get; set; }
+
+    [Required(ErrorMessage = "La Fecha de expiracion es Obligatoria")]
+    public string? fechaExpiracion { get; set; }
+
+    [Required(ErrorMessage = "El Codigo de seguridad es Obligatorio")]
     public string? codigoSeguridad { get; set; }
+
+    public Usuario? usuario { get; set; }
+
 }
