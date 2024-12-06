fbe15a63e27f185098af78348ae84447df85b0fc
Alma Ozoria <heisylior@hotmail.com>
2024-12-06 17:19:41 -0400

    creando el modelo carrusel

---
+++ Proyecto_TixPro/Models/Carrusel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_TixPro.Models;
+
+public class Carrusel
+{
+    [Key]
+    public int carruselId { get; set; }
+
+    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    public string nombre { get; set; }
+
+    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    public string imagen1 { get; set; }
+
+    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    public string imagen2 { get; set; }
+
+    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    public string imagen3 { get; set; }
+
+    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    public string imagen4 { get; set; }
+
+    [Required(ErrorMessage = "Este Campo debe de ser Obligatorio")]
+    public string imagen5 { get; set; }
+}
