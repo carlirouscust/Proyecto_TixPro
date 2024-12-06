8c8903dbed9bf0ff11384878d7914ecb0e69ec79
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-12-06 17:39:54 -0400

    se creo la migracion del carrusel

---
+++ Proyecto_TixPro/Data/Migrations/20241206213021_Carrusel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_TixPro.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Carrusel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "nombre",
+                table: "Carrusel");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "nombre",
+                table: "Carrusel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
