cfa581f7ca889177a62194497300605cf2719d81
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-12-06 20:27:23 -0400

    se creo la migracion de resenas

---
+++ Proyecto_TixPro/Data/Migrations/20241207000746_Resenas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_TixPro.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Resenas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    reviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    usuarioNombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ratingUso = table.Column<int>(type: "int", nullable: false),
+                    cosasMejorar = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    recomendarias = table.Column<bool>(type: "bit", nullable: false),
+                    fechaReview = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.reviewId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Review");
+        }
+    }
+}
