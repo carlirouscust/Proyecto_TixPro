682380f78e7960e12af715558e955d34a6c3e103
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-12-06 20:27:41 -0400

    se creo el modelo resenas

---
+++ Proyecto_TixPro/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_TixPro.Models;
+
+public class Review
+{
+    [Key]
+    public int reviewId { get; set; }
+    [Required(ErrorMessage = "El Campo es obligatorio")]
+    public string? usuarioNombre { get; set; } 
+
+    [Required(ErrorMessage = "El Campo es obligatorio")]
+    public int ratingUso { get; set; } // Puntuación (1 a 10)
+
+    [Required(ErrorMessage = "El Campo es obligatorio")]
+    public string? cosasMejorar { get; set; } 
+
+    [Required(ErrorMessage = "El Campo es obligatorio")]
+    public bool recomendarias { get; set; } 
+    public DateTime fechaReview { get; set; } = DateTime.Now; 
+}
